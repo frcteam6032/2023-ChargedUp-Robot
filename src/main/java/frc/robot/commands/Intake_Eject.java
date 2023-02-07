7b1b8a88dc8edde976a018314f3c2b999bcb2f45
FRC Team 6032 <56695163+frcteam6032@users.noreply.github.com>
2023-02-07 17:27:35 -0500

    Adds button bindings and debug prints

--- src/main/java/frc/robot/commands/Intake_Eject.java
+++ src/main/java/frc/robot/commands/Intake_Eject.java
@@ -55,11 +55,13 @@ public class Intake_Eject extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        System.out.println("Set the Motor Speed to X (Eject)");
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        System.out.println("Stop the Motor");
     }
 
     // Returns true when the command should end.
