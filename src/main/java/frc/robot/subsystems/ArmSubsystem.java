5ade70d89857ecdcbe6c7ff82e0349591ca46f7b
David Maciupa <dmm8787@gmail.com>
2023-03-17 22:03:16 -0400

    Arm shuffleboard fixes

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -55,7 +55,7 @@ public class ArmSubsystem extends SubsystemBase {
         //encoder_layout.addNumber("Absolute", m_DutyCycleEncoder::getAbsolutePosition);
         //encoder_layout.addNumber("Relative", m_DutyCycleEncoder::get);
         //encoder_layout.addNumber("Absolute*360", () -> m_DutyCycleEncoder.getAbsolutePosition() * 360);
-        encoder_layout.addNumber("Scaled 0 to 100A", () -> (79-(m_DutyCycleEncoder.get() * 90)) / 1.44).withWidget(BuiltInWidgets.kDial);
+        encoder_layout.addNumber("Scaled 0 to 100", () -> (79-(m_DutyCycleEncoder.get() * 90)) / 1.44).withWidget(BuiltInWidgets.kDial);
         encoder_layout.addNumber("Relative*90", () -> m_DutyCycleEncoder.get() * 90);
         encoder_layout.addBoolean("Is at max height", () -> (m_DutyCycleEncoder.get() * 90 <= -65));
         encoder_layout.addBoolean("Is at min height", () -> (m_DutyCycleEncoder.get() * 90 >= 79));
